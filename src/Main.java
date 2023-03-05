d96005595267f7ca41f6846c1ae64f01120d589a
GMarina14 <marika1429@gmail.com>
2023-03-05 22:07:37 +0300

    Проверьте, пожалуйста, код по базовой сложности

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,37 @@ public class Main {
     public static void main(String[] args) {
         int size = 10;
         Employee[] employees = new Employee[size];
+
+     /*Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3 *//*employees.length*//*; ++i) {
+            System.out.println("Enter the full name, the department (1-5) and the salary of an employee N" + (i + 1) + ": ");
+            employees[i] = new Employee(scanner.nextLine(), scanner.nextInt(), scanner.nextInt());
+        }
+        scanner.close();*/
+
+        //To check functionality
+
+     /* Employee[] employees = new Employee[3];
+        employees[0]= new Employee("Sara", 5, 15000);
+        employees[1]= new Employee("Nick", 1, 17000);
+        employees[2]= new Employee("Lidia", 2, 11000);
+*/
+
+        System.out.println("List of employees:");
+        Employee.listOfEmployees(employees);
+
+        System.out.println("The total costs of employees salaries per month: " + Employee.salaryCosts(employees));
+        System.out.println("Employee with the minimum wage is " + Employee.minSalaryEmployee(employees).getName());
+        System.out.println("Employee with the maximum wage is " + Employee.maxSalaryEmployee(employees).getName());
+        System.out.println("The average salary is " + Employee.avarageSalary(employees));
+        System.out.println("List of employees names:");
+        Employee.listOfEmployeesNames(employees);
+
+        System.out.println("Some info:");
+        System.out.println(employees[2].getName());
+        System.out.println(employees[2].getIdOfEmployee());
+        System.out.println(employees[2].getDepartment());
+        System.out.println(employees[2].getSalary());
     }
 }
-
